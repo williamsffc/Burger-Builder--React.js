8dc7fef5de19966f8881a06ec6a71406d968f261
Will <williamsffc@gmail.com>
2020-10-26 21:11:16 -0700

    saved

---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import classes from './Toolbar'
+
+const toolbar = (props) => {
+    <header>
+        <div>MENU</div>
+        <div>LOGO</div>
+        <nav>
+            ...
+        </nav>
+    </header>
+};
+
+export default toolbar;
